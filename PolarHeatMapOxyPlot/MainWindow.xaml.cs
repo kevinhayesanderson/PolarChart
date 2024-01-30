3ef95045e02f5f8f50b5b50dc8cfa5379b4f3525
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2024-01-30 11:14:54 +0530

    normal distribution as data

--- PolarHeatMapOxyPlot/MainWindow.xaml.cs
+++ PolarHeatMapOxyPlot/MainWindow.xaml.cs
@@ -30,10 +30,27 @@ namespace PolarHeatMapOxyPlot
             matrix[1, 0] = 1.5;
             matrix[1, 1] = 0.2;
 
+            // generate 1d normal distribution
+            var singleData = new double[100];
+            for (int x = 0; x < 100; ++x)
+            {
+                singleData[x] = Math.Exp((-1.0 / 2.0) * Math.Pow((x - 50.0) / 20.0, 2.0));
+            }
+
+            // generate 2d normal distribution
+            var data = new double[100, 100];
+            for (int x = 0; x < 100; ++x)
+            {
+                for (int y = 0; y < 100; ++y)
+                {
+                    data[y, x] = singleData[x] * singleData[(y + 30) % 100] * 100;
+                }
+            }
+
             model.Axes.Add(new AngleAxis { StartAngle = Math.PI, EndAngle = Math.PI + 360, Minimum = 0, Maximum = 360, MajorStep = 30, MinorStep = 15 });
             model.Axes.Add(new MagnitudeAxis { Minimum = 0, Maximum = 100, MajorStep = 25, MinorStep = 5 });
             model.Axes.Add(new LinearColorAxis { Position = AxisPosition.Right, Palette = OxyPalettes.Rainbow(500), HighColor = OxyColors.Gray, LowColor = OxyColors.Black });
-            model.Series.Add(new PolarHeatMapSeries { Data = matrix, Angle0 = 0, Angle1 = 360, Magnitude0 = 0, Magnitude1 = 100, Interpolate = true });
+            model.Series.Add(new PolarHeatMapSeries { Data = data, Angle0 = 0, Angle1 = 360, Magnitude0 = 0, Magnitude1 = 100, Interpolate = true });
 
             return model;
         }
